482400f07eff949d909a684e00eea9508f011407
EngSeif <eng.seifmohamed2004@gmail.com>
2024-11-07 18:30:16 +0200

    logging | Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,7 @@ Filtered Login Module
 """
 from typing import List
 import re
+import logging
 
 
 def filter_datum(fields: List[str],
@@ -16,3 +17,31 @@ def filter_datum(fields: List[str],
         r'([^' + re.escape(separator) + r']*)'
     )
     return re.sub(pattern, r'\1=' + redaction, message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """
+    Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]) -> None:
+        """
+        Class Constructor
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Filtered Message
+        """
+        formatter = logging.Formatter(self.FORMAT)
+        logMessage = formatter.format(record)
+        filtered_message = filter_datum(
+            self.fields, self.REDACTION, logMessage, self.SEPARATOR
+            )
+        return filtered_message
