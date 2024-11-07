308abf245a7335be5f4f8552a6b2d4c5ca4f37f3
EngSeif <eng.seifmohamed2004@gmail.com>
2024-11-07 18:36:41 +0200

    logging | Log formatter | fix 2

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -20,27 +20,19 @@ def filter_datum(fields: List[str],
 
 
 class RedactingFormatter(logging.Formatter):
-    """
-    Redacting Formatter class
-    """
+    """ Redacting Formatter class
+        """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields: List[str]) -> None:
-        """
-        Class Constructor
-        """
+    def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """
-        Filtered Message
-        """
-        logMessage = logging.Formatter(self.FORMAT).format(record)
-        filtered_message = filter_datum(
-            self.fields, self.REDACTION, logMessage, self.SEPARATOR
-            )
-        return filtered_message
+        """ Filters values in incoming log records using filter_datum """
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.getMessage(), self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
