caeefa998acead8239b5708bd1c7dcdb67da03c8
EngSeif <eng.seifmohamed2004@gmail.com>
2024-11-14 13:43:30 +0200

    Session Auth | SessionAuth Up2 fix

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -20,7 +20,7 @@ class SessionAuth(Auth):
             return None
         if not isinstance(user_id, str):
             return None
-        sessionId = uuid.uuid4()
+        sessionId = str(uuid.uuid4())
         self.user_id_by_session_id[sessionId] = user_id
         return sessionId
 
